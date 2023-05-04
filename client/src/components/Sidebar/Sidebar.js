1235fc19b2188adf4033c8dc5d90e0f83db49d9c
Damien Switzer <damienswitzer@hotmail.com>
2023-05-03 18:16:43 -0700

    Implement sidebar tablist using react-tabs package

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,9 +1,23 @@
 import './Sidebar.scss';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
-function Sidebar(){
+function Sidebar() {
     return (
         <section className="sidebar">
-        
+            <Tabs>
+                <TabList>
+                    <Tab>Overview</Tab>
+                    <Tab>Nodes</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <h2>Any content 1</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 2</h2>
+                </TabPanel>
+                
+            </Tabs>
         </section>
     );
 }
