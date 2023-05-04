80d59448e33c2addf012fc04fbb376fd4be79ac4
Damien Switzer <damienswitzer@hotmail.com>
2023-05-03 18:25:33 -0700

    Setup nodes state, context, and dummy data

--- client/src/components/Workspace/Workspace.js
+++ client/src/components/Workspace/Workspace.js
@@ -1,14 +1,53 @@
+import { createContext, useContext, useState } from 'react';
 import Inspector from '../Inspector/Inspector';
 import Map from '../Map/Map';
 import Sidebar from '../Sidebar/Sidebar';
 import './Workspace.scss';
 
-function Workspace(){
+const NodesContext = createContext(null);
+
+const nodesData = [
+    {
+        id: 1,
+        map_id: 1,
+        name: "Flexbox",
+        description: `Flexbox is a one-dimensional layout method for arranging items in rows or columns. Items flex (expand) to fill additional space or shrink to fit into smaller spaces. This article explains all the fundamentals.`,
+        x: 100,
+        y: 100
+    },
+    {
+        id: 2,
+        map_id: 1,
+        name: "justify-content",
+        description: `The CSS justify-content property defines how the browser distributes space between and around content items along the main-axis of a flex container, and the inline axis of a grid container.`,
+        x: 200,
+        y: 100
+    },
+    {
+        id: 3,
+        map_id: 1,
+        name: "align-items",
+        description: `The CSS align-items property sets the align-self value on all direct children as a group. In Flexbox, it controls the alignment of items on the Cross Axis. In Grid Layout, it controls the alignment of items on the Block Axis within their grid area.`,
+        x: 300,
+        y: 100
+    },
+];
+
+function Workspace() {
+    const [nodes, setNodes] = useState(null);
+
+    useState(() => {
+        setNodes(nodesData);
+    }, []);
+
+
     return (
         <main className="workspace">
-            <Sidebar/>
-            <Map/>
-            <Inspector/>
+            <NodesContext.Provider value={nodes}>
+                <Sidebar />
+                <Map />
+                <Inspector />
+            </NodesContext.Provider>
         </main>
     );
 }
