33ad7fb602afe6c02bc6ca62512820ddce5bc9bb
Damien Switzer <damienswitzer@hotmail.com>
2023-05-03 19:49:12 -0700

    Implement sheet component with pan and zoom using react-zoom-pan-pinch

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,10 +1,15 @@
 import './Map.scss';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
-function Map(){
+function Map() {
     return (
         <section className="map">
-        
-        </section>
+            <TransformWrapper>
+                <TransformComponent wrapperStyle={{ width: "100%", height: "100%", }}>
+                    <div className="map__sheet"></div>
+                </TransformComponent>
+            </TransformWrapper>
+        </section >
     );
 }
 export default Map;
