08b6cb50a1198545d789c271a3b804beee5e480a
Damien Switzer <damienswitzer@hotmail.com>
2023-05-09 07:51:54 -0700

    Add logo

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,9 +1,9 @@
 import './Navbar.scss';
-
+import logo from '../../assets/logos/MM_logo_white_shadow.png';
 function Navbar(){
     return (
         <header className="navbar">
-            <h1 className="navbar__title">Mindmap</h1>
+            <img className="navbar__logo" src={logo} alt="logo"/><h1 className="navbar__title">Mindmap</h1>
         </header>
     );
 }
