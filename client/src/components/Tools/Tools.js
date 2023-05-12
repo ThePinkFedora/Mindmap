15bea7299326060b09094ddf6b003ce08e914786
Damien Switzer <damienswitzer@hotmail.com>
2023-05-12 12:33:27 -0700

    Implement tools window tooltips

--- client/src/components/Tools/Tools.js
+++ client/src/components/Tools/Tools.js
@@ -18,9 +18,9 @@ import unlinkIcon from '../../assets/images/unlink.svg';
 function Tools({ onAdd, onDelete, onLink, onUnlink }) {
     const links = useContext(LinksContext);
     /** @type {{selection:import('../../js/nodemaps').Selections}} */
-    const {
-        selection
-    } = useContext(SelectionContext);
+    const { selection } = useContext(SelectionContext);
+    const [tooltip, setTooltip] = useState(null);
+
 
     const handleAdd = (event) => {
         onAdd();
@@ -39,38 +39,85 @@ function Tools({ onAdd, onDelete, onLink, onUnlink }) {
         onUnlink(link.id);
     };
 
+    /**
+     * @param {Event} event 
+     * @param {string} tool 
+     */
+    const handleHover = (event, tool, hovered = true) => {
+        switch (tool) {
+            case 'add':
+                setTooltip('Add');
+                break;
+            case 'remove':
+                setTooltip('Delete (Requires Selection)');
+                break;
+            case 'link':
+                setTooltip('Link (Select 2 Nodes)');
+                break;
+            case 'unlink':
+                setTooltip('Unlink (Select 2 Nodes)');
+                break;
+            default:
+                setTooltip(null);
+                break;
+        }
+    };
+
     return (
         <div className="tools">
-            <div className="tools__window">
+            <div className="tools__window" onMouseLeave={(e) => handleHover(e, null)}>
                 <div className="tools__header">
                     <h1 className="tools__title">Tools</h1>
                 </div>
                 <div className="tools__content">
                     <ul className="tools__list">
-                        <li className="tools__item">
-                            <button className='tools__button' onClick={handleAdd}>
+                        <li className="tools__item"
+                            onMouseEnter={(e) => handleHover(e, 'add')}
+                        >
+                            <button className='tools__button' onClick={handleAdd} >
                                 <img className="tools__icon" src={addIcon} alt="add" /> Add Node
                             </button>
                         </li>
-                        <li className="tools__item">
+                        <li className="tools__item"
+                            onMouseEnter={(e) => handleHover(e, 'remove')}
+                        >
                             <button className='tools__button' onClick={handleDelete} disabled={!selection.length}>
                                 <img className="tools__icon" src={removeIcon} alt="remove" /> Delete Node
                             </button>
                         </li>
-                        <li className="tools__item">
+                        <li className="tools__item"
+                            onMouseEnter={(e) => handleHover(e, 'link')}
+                        >
                             <button className='tools__button' onClick={handleLink} disabled={selection.length !== 2}>
                                 <img className="tools__icon" src={linkIcon} alt="link" /> Link Nodes
+
                             </button>
                         </li>
-                        <li className="tools__item">
+                        <li className="tools__item"
+                            onMouseEnter={(e) => handleHover(e, 'unlink')}
+                        >
                             <button className='tools__button' onClick={handleUnlink} disabled={selection.length !== 2}>
                                 <img className="tools__icon" src={unlinkIcon} alt="unlink" /> Unlink Nodes
                             </button>
                         </li>
                     </ul>
                 </div>
+                {tooltip &&
+                    <div className="tools__tooltip tools__tooltip--visible">
+                        {tooltip}
+                    </div>
+                }
             </div>
         </div>
     );
 }
+
+// function Tooltip({ message }) {
+//     return (
+//         <div className='tooltip'>
+//             <span className="tooltip__message">{message}</span>
+//         </div>
+//     )
+// }
+
 export default Tools;
